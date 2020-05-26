ca9521715a9f83463dbbbd2c2919c4b544896338
Arata Tanaka <tarata43@yahoo.co.jp>
2020-05-26 20:46:48 +0900

    Add xcode project

---
+++ swiftui-getting-started/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-getting-started
+//
+//  Created by 田中改 on 2020/05/26.
+//  Copyright © 2020 aratanaka. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
